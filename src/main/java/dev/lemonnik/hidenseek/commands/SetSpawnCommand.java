7de763f9ff629c8ba5e93afe1860ad588dbe784f
lemonnik <lemonnik.0@gmail.com>
2026-06-30 23:20:54 +0200

    better setspawn

--- src/main/java/dev/lemonnik/hidenseek/commands/SetSpawnCommand.java
+++ src/main/java/dev/lemonnik/hidenseek/commands/SetSpawnCommand.java
@@ -3,6 +3,7 @@ package dev.lemonnik.hidenseek.commands;
 import dev.lemonnik.hidenseek.utils.World;
 import dev.lemonnik.hidenseek.utils.WorldManager;
 import net.minestom.server.command.builder.Command;
+import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
 
 public class SetSpawnCommand extends Command {
@@ -17,7 +18,12 @@ public class SetSpawnCommand extends Command {
              if (sender instanceof Player player) {
                  World world = WorldManager.getWorld(player.getInstance());
                  if (world != null) {
-                 WorldManager.setWorldSpawn(world.id(), player.getPosition());
+                 Pos pos = player.getPosition();
+                 WorldManager.setWorldSpawn(world.id(), new Pos(
+                         Math.floor(pos.x()) + 0.5,
+                         Math.floor(pos.y()),
+                         Math.floor(pos.z()) + 0.5
+                 ));
                  }
              }
          });

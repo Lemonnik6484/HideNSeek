79fd613b282ff784fd488ae3875a4d8ba1de3573
JXSnack <jx.snack@gmail.com>
2026-07-03 01:05:43 +0200

    shuffle weighter pool

--- src/main/java/dev/lemonnik/hidenseek/SessionManager.java
+++ src/main/java/dev/lemonnik/hidenseek/SessionManager.java
@@ -20,13 +20,7 @@ import net.minestom.server.timer.Scheduler;
 import net.minestom.server.timer.Task;
 import net.minestom.server.timer.TaskSchedule;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SessionManager {
@@ -182,6 +176,8 @@ public class SessionManager {
                 weightedPool.add(player);
             }
         }
+
+        Collections.shuffle(weightedPool);
 
         int seekerCount = getSeekerCount(onlinePlayers.size());
         for (int i = 0; i < seekerCount; i++) {

eb35cb7d15fafe00af28eb800d334873a7c414b2
JXSnack <jx.snack@gmail.com>
2026-07-03 00:56:45 +0200

    tp command

---
+++ src/main/java/dev/lemonnik/hidenseek/commands/TeleportCommand.java
@@ -0,0 +1,39 @@
+package dev.lemonnik.hidenseek.commands;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.CommandExecutor;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.command.builder.arguments.number.ArgumentDouble;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import org.jetbrains.annotations.Nullable;
+
+public class TeleportCommand extends Command {
+    public TeleportCommand() {
+        super("teleport", "tp");
+
+        ArgumentDouble xArgument = ArgumentType.Double("x");
+        ArgumentDouble yArgument = ArgumentType.Double("y");
+        ArgumentDouble zArgument = ArgumentType.Double("z");
+
+        addSyntax((sender, ctx) -> {
+            if (!(sender instanceof Player player)) return;
+
+            double x = ctx.get(xArgument);
+            double y = ctx.get(yArgument);
+            double z = ctx.get(zArgument);
+
+            player.teleport(player.getPosition().withCoord(x, y, z));
+            player.sendMessage("Teleported to %s %s %s".formatted(x, y, z));
+        });
+    }
+
+    @Override
+    public void setDefaultExecutor(@Nullable CommandExecutor sender) {
+        if (!(sender instanceof Player player)) return;
+        player.sendMessage("You are a MORON!!!!!!!!!");
+    }
+
+
+}
